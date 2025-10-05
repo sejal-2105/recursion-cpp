0b62bac44ad3e440998dec316d46c2ac63bfa568
sejal-2105 <sejalsingh5216@gmail.com>
2025-10-05 11:35:37 +0530

    Create p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int fact(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else 
+        return n * fact(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number here: ";
+    cin >> n;
+    cout << n << "! = " << fact(n);
+
+    return 0;
+}
